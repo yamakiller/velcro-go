2573e482498eb0f0ca380ee7e978c39fd02afc9e
yamakiller <296270991@qq.com>
2024-01-05 11:43:06 +0800

    行为树

---
+++ behavior/behavior.go
@@ -0,0 +1,18 @@
+package behavior
+
+const (
+	COMPOSITE = "composite"
+	DECORATOR = "decorator"
+	ACTION    = "action"
+	CONDITION = "condition"
+)
+
+// Returning status
+type Status uint8
+
+const (
+	SUCCESS Status = 1
+	FAILURE Status = 2
+	RUNNING Status = 3
+	ERROR   Status = 4
+)
---
+++ behavior/core/blackboard.go
@@ -0,0 +1,2 @@
+// 黑板
+package core
