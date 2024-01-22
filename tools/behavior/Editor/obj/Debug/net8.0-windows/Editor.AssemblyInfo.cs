bd417471b636de4bdff7114bd9980eeed93991ca
冷帆520 <11615645+cold-sail-520@user.noreply.gitee.com>
2024-01-22 11:25:44 +0800

    111

--- tools/behavior/Editor/obj/Debug/net8.0-windows/Editor.AssemblyInfo.cs
+++ tools/behavior/Editor/obj/Debug/net8.0-windows/Editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efd228d3d15426eddbc440abf1e9fbca31633a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tools/behavior/NodeView/obj/Debug/net8.0-windows/NodeBehavior.AssemblyInfo.cs
+++ tools/behavior/NodeView/obj/Debug/net8.0-windows/NodeBehavior.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NodeBehavior")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efd228d3d15426eddbc440abf1e9fbca31633a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NodeBehavior")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NodeBehavior")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
