407faba39741284b99e8e93391d884b73a84119f
yamakiller <296270991@qq.com>
2024-01-23 14:40:23 +0800

    询问对话框

--- tools/behavior/Editor/Commands/SaveWorkspaceCommand.cs
+++ tools/behavior/Editor/Commands/SaveWorkspaceCommand.cs
@@ -1,23 +1,21 @@
 ﻿using Editor.Framework;
 using Editor.ViewModels;
-using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Editor.Commands
 {
-    class ExitSystemCommand : ViewModelCommand<EditorFrameViewModel>
+    class SaveWorkspaceCommand : ViewModelCommand<EditorFrameViewModel>
     {
-        public ExitSystemCommand(EditorFrameViewModel contextViewModel) : base(contextViewModel)
+        public SaveWorkspaceCommand(EditorFrameViewModel contextViewModel) : base(contextViewModel)
         {
         }
 
         public override void Execute(EditorFrameViewModel contextViewModel, object parameter)
         {
-            if (contextViewModel.IsModifyed)
-            {
-
-            }
-
-            Application.Current.Shutdown();
         }
 
         public override bool CanExecute(EditorFrameViewModel contextViewModel, object parameter)
---
+++ tools/behavior/Editor/Dialogs/WhatDialog.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Editor.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Editor.Dialogs
+{
+    /// <summary>
+    /// WhatDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class WhatDialog : Window
+    {
+        public static bool ShowWhatMessage(string caption , string message )
+        {
+            WhatDialog wdlg = new WhatDialog();
+            wdlg.Caption = caption;
+            wdlg.Message = message;
+
+            var result = wdlg.ShowDialog();
+
+            return result == true ? true : false;
+        }
+        public WhatDialog()
+        {
+            InitializeComponent();
+            DataContext = new WhatDialogViewModel();
+        }
+
+        public string Caption
+        {
+            set
+            {
+                ((WhatDialogViewModel)DataContext).Caption = value;
+            }
+        }
+
+        public string Message
+        {
+            set
+            {
+                ((WhatDialogViewModel)DataContext).Message = value;
+            }
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
